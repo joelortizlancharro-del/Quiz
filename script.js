fd2cf8c02ffcacb9366e94f01c0da000b4e779b5
Pepe Sufragio <albertkstyon@gmail.com>
2026-04-14 19:53:57 +0200

    questions fetch

--- script.js
+++ script.js
@@ -16,14 +16,21 @@ const cargarPreguntes = async () => {
     try {
         const res = await fetch("data.json");
         preguntes = await res.json();
-
+        console.log(preguntes);
     } catch (error) {
         console.error("Error cargando JSON:", error);
     }
 }
-
+cargarPreguntes();
+const mostrarPregunta = (preguntes) => {
+    console.log(preguntes)
+}
 
 
 const iniciarJoc = () => {
-
+    let indice = 0;
+    let puntuacion = 0;
+    mostrarPregunta(preguntes);
 }
+
+iniciarJoc();
