bd2fb54852303fa226a61ab3d84ec02dee390f1c
Pepe Sufragio <albertkstyon@gmail.com>
2026-04-20 19:42:07 +0200

    bucle per mostrar preguntes

--- script.js
+++ script.js
@@ -10,8 +10,16 @@ confirmBtn.addEventListener('click', () => {
     nom = nameInput.value
 })
 */
-let preguntes = [];
-
+let preguntes;
+let indice = 0;
+let puntuacion = 0;
+const mode = {
+    pokemon: "preguntesPokemon",
+    classics: "preguntesClassics",
+    shooters: "preguntesShooters",
+    cultura: "preguntesCulturaGeneral",
+    caos: "caos"
+}
 const cargarPreguntes = async () => { 
     try {
         const res = await fetch("data.json");
@@ -20,17 +28,61 @@ const cargarPreguntes = async () => {
     } catch (error) {
         console.error("Error cargando JSON:", error);
     }
-}
-cargarPreguntes();
-const mostrarPregunta = (preguntes) => {
-    console.log(preguntes)
+    iniciarJoc();
 }
 
+const mostrarPregunta = (question) => {
+    const questionCard = document.getElementById("tarjeta");
+
+        // limpiar contingut anterior
+        questionCard.innerHTML = "";
+
+        // crear h3 amb la pregunta
+        const h3 = document.createElement("h3");
+        h3.textContent = question.pregunta;
+
+        questionCard.appendChild(h3);
+
+        // crear opcions
+        question.opcions.forEach(opcion => {
+            const p = document.createElement("p");
+            p.textContent = opcion;
+
+            questionCard.appendChild(p);
+        });
+
+}
+
+const comprobarRespuesta = (respuestaUsuario, respuestaCorrecta) => {
+    if (respuestaUsuario === respuestaCorrecta) {
+        puntuacion += 100;
+    }else {
+        puntuacion -= 30;
+    }
+
+    indice++; // 🔥 avanzar a la siguiente
+    siguientePregunta();
+};
+
+const siguientePregunta = () => {
+    if (indice < preguntas.length) {
+        mostrarPregunta(preguntas[indice]);
+    } else {
+        terminarJuego();
+    }
+};
+
+const terminarJuego = () => {
+    const contenedor = document.getElementById("tarjeta");
+    contenedor.innerHTML = `<h2>Juego terminado</h2>
+                            <p>Puntuación: ${puntuacion}</p>`;
+};
 
 const iniciarJoc = () => {
-    let indice = 0;
-    let puntuacion = 0;
-    mostrarPregunta(preguntes);
+    indice = 0;
+    puntuacion = 0;
+    seguentPregunta();
 }
 
-iniciarJoc();
+
+cargarPreguntes();
